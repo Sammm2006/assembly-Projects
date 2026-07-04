9926ee56daba36f104f372c4fccfab1289972683
Sasindu <sasindupabasara1@gmail.com>
2026-07-04 22:14:35 +0530

    One Digit Addision

--- Calculator/calc.s
+++ Calculator/calc.s
@@ -2,24 +2,94 @@
 .intel_syntax noprefix
 
 _start:
-       //read input 
+       mov rax, 1
+       mov rdi, 1
+       lea rsi, [menu]
+       mov rdx, 82
+       syscall
+
        mov rax, 0
        mov rdi, 0
-       lea rsi, [buffer]
+       lea rsi, [option]
+       mov rdx, 5
+       syscall
+
+       mov al, [option]
+       sub al, '0'
+       mov bl, al
+
+       mov rax, 1
+       mov rdi, 1
+       lea rsi, [firstO]
+       mov rdx, 23
+       syscall
+
+       mov rax, 0
+       mov rdi, 0
+       lea rsi, [firstNum]
        mov rdx, 15
        syscall
 
        mov rax, 1
        mov rdi, 1
-       lea rsi, [buffer]
+       lea rsi, [secondO]
+       mov rdx, 24
+       syscall
+
+       mov rax, 0
+       mov rdi, 0
+       lea rsi, [secondNum]
        mov rdx, 15
        syscall
 
+       cmp bl, 1
+       je Addition
+
+Addition: 
+       movzx rbx, byte ptr [firstNum]
+       sub rbx, '0'
+       movzx rbp, byte ptr [secondNum]
+       sub rbp, '0'
+       add rbx, rbp
+       add rbx, '0'
+       mov [equal], rbx
+       syscall
+
+       mov rax, 1
+       mov rdi, 1
+       lea rsi, [equal]
+       mov rdx, 5
+       syscall
+       
        mov rax, 60
        mov rdi, 69
        syscall
 
+.section .data
+menu:
+    .asciz "Calculater!\n1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n\nYour Choice : "
+
+.section .data
+firstO:
+    .asciz "\nEnter First Number : "
+
+.section .data
+secondO:
+    .asciz "\nEnter Second Number : "
+
 .section .bss
-buffer:
-    .skip 15
+option: 
+    .skip 5
+
+.section .bss
+equal: 
+    .skip 5
+
+.section .bss
+firstNum: 
+    .skip 10
+
+.section .bss
+secondNum: 
+    .skip 10
 
--- assembly.md
+++ assembly.md
@@ -1,3 +1,6 @@
+## SysCall Table
+[Linux System Call Table](https://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64/)
+
 ## Initialized and Uninitiazited Memory
 ### Initialized
 .section .data <br>
